77731c20bf2474e018a1544184e636365569c690
jared <jcapper@student.sierracollege.edu>
2019-03-27 18:27:29 -0700

    Added my first and last name, line 10.

--- Hello.java
+++ Hello.java
@@ -7,7 +7,7 @@ public class Hello {
 		// Add a new line with your information
 		System.out.println("219501959 - This line was added by Rigoberto Garcia.");
 		System.out.println("SacStateID - This line was added by First name Last name.");
-		System.out.println("SacStateID - This line was added by First name Last name.");
+		System.out.println("214570071 - This line was added by Jared Capper.");
 		System.out.println("SacStateID - This line was added by First name Last name.");
 		System.out.println("SacStateID - This line was added by First name Last name.");
 		System.out.println("SacStateID - This line was added by First name Last name.");
